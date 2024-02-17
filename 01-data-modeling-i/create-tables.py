88af79cbe269cc6fb0e89bf9d6cabdeb596d0cb6
Phanpaporn Z. <32537059+sueshi-lab@users.noreply.github.com>
2024-02-17 18:04:28 +0000

    Create more tables - repos, orgs, event_types

--- 01-data-modeling-i/create-tables.py
+++ 01-data-modeling-i/create-tables.py
@@ -8,31 +8,73 @@ PostgresConn = NewType("PostgresConn", psycopg2.extensions.connection)
 
 table_drop_events = "DROP TABLE IF EXISTS events"
 table_drop_actors = "DROP TABLE IF EXISTS actors"
+table_drop_repos = "DROP TABLE IF EXISTS repos"
+table_drop_orgs = "DROP TABLE IF EXISTS orgs"
+table_drop_event_types = "DROP TABLE IF EXISTS event_types"
 
 table_create_actors = """
     CREATE TABLE IF NOT EXISTS actors (
         id int,
         login text,
+        display_login text,
+        url text,
+        avatar_url text,
+        PRIMARY KEY(id)
+    )
+"""
+table_create_repos = """
+    CREATE TABLE IF NOT EXISTS repos (
+        id int,
+        name text,
+        url text,
+        PRIMARY KEY(id)
+    )
+"""
+table_create_orgs = """
+    CREATE TABLE IF NOT EXISTS orgs (
+        id int,
+        login text,
+        url text,
+        avatar_url text,
         PRIMARY KEY(id)
     )
 """
 table_create_events = """
     CREATE TABLE IF NOT EXISTS events (
-        id text,
-        type text,
+        id BIGINT,
+        type_id int,
         actor_id int,
+        repo_id int,
+        org_id int,
+        is_public boolean,
+        created_at timestamp,
         PRIMARY KEY(id),
-        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id)
+        CONSTRAINT fk_type FOREIGN KEY(type_id) REFERENCES event_types(id),
+        CONSTRAINT fk_actor FOREIGN KEY(actor_id) REFERENCES actors(id),
+        CONSTRAINT fk_repo FOREIGN KEY(repo_id) REFERENCES repos(id),
+        CONSTRAINT fk_org FOREIGN KEY(org_id) REFERENCES orgs(id)
+    )
+"""
+table_create_event_types = """
+    CREATE TABLE IF NOT EXISTS event_types (
+        id SERIAL PRIMARY KEY,
+        name text
     )
 """
 
 create_table_queries = [
     table_create_actors,
+    table_create_repos,
+    table_create_orgs,
+    table_create_event_types,
     table_create_events,
 ]
 drop_table_queries = [
     table_drop_events,
     table_drop_actors,
+    table_drop_repos,
+    table_drop_orgs,
+    table_drop_event_types,
 ]
 
 
--- 01-data-modeling-i/etl.py
+++ 01-data-modeling-i/etl.py
@@ -55,24 +55,112 @@ def process(cur, conn, filepath):
                         each["created_at"],
                     )
 
-                # Insert data into tables here
+                # Insert data into actors
                 insert_statement = f"""
                     INSERT INTO actors (
                         id,
-                        login
-                    ) VALUES ({each["actor"]["id"]}, '{each["actor"]["login"]}')
+                        login,
+                        display_login,
+                        url,
+                        avatar_url
+                    ) 
+                    VALUES (
+                        {each["actor"]["id"]}, 
+                        '{each["actor"]["login"]}', 
+                        '{each["actor"]["display_login"]}', 
+                        '{each["actor"]["url"]}', 
+                        '{each["actor"]["avatar_url"]}'
+                    )
                     ON CONFLICT (id) DO NOTHING
                 """
                 # print(insert_statement)
                 cur.execute(insert_statement)
 
-                # Insert data into tables here
+                # Insert data into repos
                 insert_statement = f"""
-                    INSERT INTO events (
+                    INSERT INTO repos (
                         id,
-                        type,
-                        actor_id
-                    ) VALUES ('{each["id"]}', '{each["type"]}', '{each["actor"]["id"]}')
+                        name,
+                        url
+                    ) 
+                    VALUES (
+                        {each["repo"]["id"]}, 
+                        '{each["repo"]["name"]}',  
+                        '{each["repo"]["url"]}'
+                    )
+                    ON CONFLICT (id) DO NOTHING
+                """
+                # print(insert_statement)
+                cur.execute(insert_statement)
+
+                # Insert data into orgs
+                if "org" in each:
+                    insert_statement = f"""
+                        INSERT INTO orgs (
+                            id,
+                            login,
+                            url,
+                            avatar_url
+                        ) 
+                        VALUES (
+                            {each["org"]["id"]}, 
+                            '{each["org"]["login"]}',  
+                            '{each["org"]["url"]}',  
+                            '{each["org"]["avatar_url"]}'
+                        )
+                        ON CONFLICT (id) DO NOTHING
+                    """
+                else:
+                    print("No 'org' information found in the object.", each["id"])
+                # print(insert_statement)
+                cur.execute(insert_statement)
+
+                # Insert data into event_types
+                insert_statement = f"""
+                    INSERT INTO event_types (
+                        name
+                    ) 
+                    SELECT '{each["type"]}'
+                    WHERE NOT EXISTS (
+                        SELECT 1 FROM event_types WHERE name = '{each["type"]}'
+                    )
+                    RETURNING id
+                """
+                # print(insert_statement)
+                cur.execute(insert_statement)
+
+                # Finding event type id
+                type_id_row = cur.fetchone()
+                if type_id_row:
+                    type_id = type_id_row[0]
+                else:
+                    select_event_type_statement = f"""
+                        SELECT id FROM event_types WHERE name = '{each["type"]}'
+                    """
+                    cur.execute(select_event_type_statement)
+                    type_id_row = cur.fetchone()
+                    type_id = type_id_row[0] if type_id_row else None
+                print(type_id)
+
+                # Insert data into events
+                insert_statement = f"""
+                INSERT INTO events (
+                        id,
+                        type_id,
+                        actor_id,
+                        repo_id,
+                        org_id,
+                        is_public,
+                        created_at
+                    ) VALUES (
+                        {each["id"]}, 
+                        {type_id}, 
+                        {each["actor"]["id"]},
+                        {each["repo"]["id"]}, 
+                        {'NULL' if "org" not in each or "id" not in each["org"] else each["org"]["id"]},
+                        {each["public"]}, 
+                        '{each["created_at"]}'
+                    )
                     ON CONFLICT (id) DO NOTHING
                 """
                 # print(insert_statement)
