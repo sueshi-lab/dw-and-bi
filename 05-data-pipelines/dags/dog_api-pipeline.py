f2756d86f89b0abfdd026b659cf38a3e86669ec2
Phanpaporn Z. <32537059+sueshi-lab@users.noreply.github.com>
2024-05-05 11:00:29 +0000

    Refactor (05) : dog_api_pipeline.py

--- 05-data-pipelines/dags/dog_api-pipeline.py
+++ 05-data-pipelines/dags/dog_api-pipeline.py
@@ -15,10 +15,6 @@ def _get_dog_api():
     with open("/opt/airflow/dags/dog.json","w") as f:
         json.dump(data, f)
 
-def _move_file():
-    import os
-    os.system("mv /opt/airflow/dags/dog.json /path/to/doggy/")
-
 with DAG(
     "dog_api_pipeline",
     start_date=timezone.datetime(2024, 3, 23),
@@ -30,11 +26,6 @@ with DAG(
     get_dog_api = PythonOperator(
         task_id="get_dog_api",
         python_callable=_get_dog_api,
-    )
-
-    move_file = BashOperator(
-        task_id="move_file",
-        bash_command="_move_file",
     )
 
     end = EmptyOperator(task_id="end")
